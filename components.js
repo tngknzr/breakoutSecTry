a1cc9b1cbedef1773634b849f29518c03e52b912
Agustin Paredes <nomegustabergamo@outlook.com>
2023-06-12 17:50:13 +0200

    other commi

--- components.js
+++ components.js
@@ -2,3 +2,13 @@
 
 const bg_img = new Image();
 bg_img.src = "img/bg.jpg";
+
+const level_img = new Image();
+level_img.src = "img/level.png";
+
+const score_img = new Image();
+score_img.src = "img/score.png";
+const life_img = new Image();
+life_img.src = "img/life.png";
+
+
--- game.js
+++ game.js
@@ -6,6 +6,10 @@ let leftArrow = false;
 let rightArrow = false;
 const BALL_RADIUS = 8;
 let LIFE = 3;
+let SCORE = 0;
+let SCORE_UNIT = 10;
+let LEVEL = 1;
+const MAX_LEVEL = 3;
 
 
 // game variables and constants
@@ -69,10 +73,7 @@ const ball = {
 //draw the ball
 function drawBall() {
     ctx.beginPath();
-
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "red";
-    ctx.fill();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
     ctx.strokeStyle = "blue";
     ctx.stroke();
     ctx.closePath();
@@ -86,12 +87,78 @@ function moveBall() {
 }
 
 
-
+// create the brick
+const brick = {
+    row: 3,
+    colum: 5,
+    width: 55,
+    height: 20,
+    offSetLeft: 20,
+    offSetTop: 20,
+    marginTop: 40,
+    fillColor: "#2e3548",
+    strokeColor: "#fff"
+}
+let bricks = [];
+function createBricks() {
+    for (let r = 0; r < brick.row; r++) {
+        bricks[r] = [];
+        for (let c = 0; c < brick.colum; c++) {
+            bricks[r][c] = {
+                x: c * (brick.offSetLeft + brick.width) + brick.offSetLeft,
+                y: r * (brick.offSetTop + brick.height) + brick.offSetTop + brick.marginTop,
+                status: true
+            }
+        }
+    }
+}
+createBricks();
+//drow the bricks
+function drawBricks() {
+    for (let r = 0; r < brick.row; r++) {
+        for (let c = 0; c < brick.colum; c++) {
+            let b = bricks[r][c];
+            if (b.status) {
+                ctx.fillStyle = brick.fillColor;
+                ctx.fillRect(b.x, b.y, brick.width, brick.height);
+                ctx.strokeStyle = brick.strokeColor;
+                ctx.strokeRect(b.x, b.y, brick.width, brick.height);
+            }
+        }
+    }
+}
+//ball bric collision
+function ballBrickCollision() {
+    for (let r = 0; r < brick.row; r++) {
+        for (let c = 0; c < brick.colum; c++) {
+            let b = bricks[r][c];
+            if (b.status) {
+                if (ball.x + ball.radius > b.x && ball.x - ball.radius < b.x + brick.width && ball.y + ball.radius > b.y && ball.y - ball.radius < b.y + brick.height) {
+                    ball.dy = - ball.dy;
+                    b.status = false;
+                    SCORE += SCORE_UNIT;
+                }
+            }
+        }
+    }
+}
+//show gamestats
+function showGameStats(text, textX, textY, img, imgX, imgY) {
+    //draw text
+    ctx.fillStyle = "#fff";
+    ctx.font = "25px Germania One";
+    ctx.fillText(text, textX, textY);
+    ctx.drawImage(img, imgX, imgY, width = 25, height = 25);
+}
 
 // DRAW FUNCTION
 function draw() {
     drawPaddle();
-    drawBall()
+    drawBall();
+    drawBricks();
+    showGameStats(SCORE, 35, 25, score_img, 5, 5);
+    showGameStats(LIFE, cvs.width - 25, 25, life_img, cvs.width - 55, 5);
+    showGameStats(LEVEL, cvs.width / 2, 25, level_img, cvs.width / 2 - 30, 5);
 
 
 
@@ -132,12 +199,13 @@ function ballPaddleCollision() {
     }
 }
 
-//UPDATE FAME FUNCTION 
+//UPDATE gAME FUNCTION 
 function update() {
     movePaddle()
     moveBall()
     ballWallCollision()
     ballPaddleCollision()
+    ballBrickCollision()
 
 }
 
